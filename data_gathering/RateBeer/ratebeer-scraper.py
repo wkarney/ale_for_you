8edf5e55b551bc8026ed9204a5467c821077dfc1
Will Karnasiewicz <willkarnasiewicz@gmail.com>
2019-05-12 18:21:55 -0400

    web scraping address info

--- data_gathering/RateBeer/ratebeer-scraper.py
+++ data_gathering/RateBeer/ratebeer-scraper.py
@@ -10,8 +10,28 @@ driver = webdriver.Chrome(executable_path='../chromedriver')
 
 arealist =[
     {
+    'url':'https://www.ratebeer.com/breweries/washington-dc/48/213/',
+    'name': 'DC'
+    },
+    {
+    'url':'https://www.ratebeer.com/breweries/delaware/8/213/',
+    'name': 'DE'
+    },
+    {
     'url':'https://www.ratebeer.com/breweries/maryland/20/213/',
     'name': 'MD'
+    },
+    {
+    'url':'https://www.ratebeer.com/breweries/pennsylvania/38/213/,
+    'name': 'PA'
+    },
+    {
+    'url':'https://www.ratebeer.com/breweries/virginia/46/213/',
+    'name': 'VA'
+    },
+    {
+    'url':'https://www.ratebeer.com/breweries/west%20virginia/49/213/',
+    'name': 'WV'
     }]
 
 for state in arealist:
@@ -24,6 +44,7 @@ for state in arealist:
     table = soup.find('table',{'id':'brewerTable'})
 
     region_brews = []
+    
 
     for i in range(len(table.find_all('a'))):
         brewery = {}
@@ -36,15 +57,40 @@ for state in arealist:
 
     allbrews = []
 
-    for brewery in region_brews:
+    for brewery in region_brews[:1]:
         
         driver.get(brewery['link'])
         sleep(10)
         html = driver.page_source
         soup = BeautifulSoup(html, 'lxml')
         
+        try:
+            brewery['google_map_link'] = soup.find('a',{'title':'View beermap'})['href'][2:]
+        except:
+            brewery['google_map_link'] = None
+        try:
+            brewery['address'] = soup.find('a',{'title':'View beermap'}).find_all('span')[0].text
+        except:
+            brewery['address'] = None
+        try:
+            brewery['town'] = soup.find('a',{'title':'View beermap'}).find_all('span')[1].text
+        except:
+            brewery['town'] = None
+        try:
+            brewery['state'] = soup.find('a',{'title':'View beermap'}).find_all('span')[2].text
+        except:
+            brewery['state'] = None
+        try:
+            brewery['country'] = soup.find('a',{'title':'View beermap'}).find_all('span')[3].text
+        except:
+            brewery['country'] = None
+        try:
+            brewery['postal_code'] = soup.find('a',{'title':'View beermap'}).find_all('span')[4].text
+        except:
+            brewery['postal_code'] = None
+
         table = soup.find('table',{'id':'brewer-beer-table'})
-        
+
         beermenu = []
         try:
             table.find('tbody').find_all('tr')
@@ -55,17 +101,23 @@ for state in arealist:
                 sleep(10)
                 html = driver.page_source
                 soup = BeautifulSoup(html, 'lxml')
-        
+
                 table = soup.find('table',{'id':'brewer-beer-table'})
             except:
                 continue
-            
+
         for row in table.find('tbody').find_all('tr'):
             beer = {}
-            beer['brewery'] = brewery['name']
             beer['name'] = row.find('a').text
             beer['style'] = row.find('span').text
             beer['link'] = f"www.ratebeer.com{row.find('a')['href']}"
+            beer['brewery'] = brewery['name']
+            beer['google_map_link'] = brewery['google_map_link']
+            beer['address'] = brewery['address']
+            beer['town'] = brewery['town']
+            beer['state'] = brewery['state']
+            beer['country'] = brewery['country']
+            beer['postal_code'] = brewery['postal_code']
             try:
                 beer['style_rating'] = float(row.find_all('td')[-2].text)
             except:
@@ -93,6 +145,6 @@ for state in arealist:
 
     brewlist = pd.DataFrame(allbrews)
 
-    brewlist.to_csv(f'./data/brewlist_{state["name"]}.csv',index=False)
+    brewlist.to_csv(f'../../data/brewlist_{state["name"]}.csv',index=False)
 
 driver.close()
